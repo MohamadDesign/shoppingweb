08ae00508f9ebba380936bff76db5d3dd1e2d23d
Mohammad <mohammadenglish19@gmail.com>
2023-07-06 12:53:29 +0330

    base project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Slider from "./Components/Slider/Slider";
+import Why from "./Components/Why/Why";
+import Arrival from "./Components/Arrival/Arrival";
+import Product from "./Components/Products/Products";
+import Subscribe from "./Components/Subscribe/Subscribe";
+import Client from "./Components/Client/Client";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="hero-area">
+        <Header />
+        <Slider />
+        <Why />
+        <Arrival />
+        <Product />
+        <Subscribe />
+        <Client />
+        <Footer />
+      </div>
+    </>
   );
 }
 
